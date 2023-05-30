151e871ea5229d06f8a6c0a84b6f98ac97286820
G. Sun (RS - Woodland) <gs534@melete.eng.cam.ac.uk>
2023-05-30 08:53:19 +0100

    instructions for paths

--- egs/librispeech/aed1/train.sh
+++ egs/librispeech/aed1/train.sh
@@ -2,9 +2,9 @@ export PYTHONPATH="/scratch/OpenSource/espnet:$PYTHONPATH"
 export PYTHONPATH="/scratch/OpenSource/espnet/espnet/nets/pytorch_backend/lm:$PYTHONPATH"
 . ./path.sh || exit 1;
 
-train_config=conf/train_TCPGen_100.yaml
+train_config=conf/train_KB_conformer.yaml
 backend=pytorch
-expname=Librispeech_AED_TCPGen_GCNII2L
+expname=Librispeech_AED_TCPGen_Comb_GCN_Sage
 preprocess_config=conf/specaug_tm.yaml
 
 # lexicon_path=/home/dawna/gs534/espnet-KG/egs/ami/ami_lextree/data/local/wordlm_train/rarewords.txt
@@ -13,9 +13,10 @@ debugmode=1
 bpemode=unigram
 nbpe=600
 suffix='suffix'
-KB='_KB'
+KB='_KBf120'
 context=''
-dict=/data/mifs_scratch/gs534/librispeech/data_100/lang_char/train_960_${bpemode}${nbpe}${suffix}_units.txt
+# Change to your own dict path
+dict=/data/mifs_scratch/gs534/librispeech/data/lang_char/train_960_${bpemode}${nbpe}${suffix}_units.txt
 # dict=data/lang_1char/ihm_train_units.txt
 mkdir -p ${expdir}
 
@@ -32,5 +33,7 @@ python ../../../espnet/bin/asr_train.py \
     --minibatches 0 \
     --verbose 0 \
     --seed 1 \
-    --train-json /data/mifs_scratch/gs534/librispeech/dump/train_100/deltafalse/data_${bpemode}${nbpe}${suffix}${KB}${context}.json \
-    --valid-json /data/mifs_scratch/gs534/librispeech/dump/dev_100/deltafalse/data_${bpemode}${nbpe}${suffix}${KB}${context}.json \
+    --train-json /data/mifs_scratch/gs534/librispeech/dump/train/deltafalse/data_${bpemode}${nbpe}${suffix}${KB}${context}.json \
+    --valid-json /data/mifs_scratch/gs534/librispeech/dump/dev/deltafalse/data_${bpemode}${nbpe}${suffix}${KB}${context}.json \
+
+# You need to change train and valid to your own dumped json files, see examples in dump/
